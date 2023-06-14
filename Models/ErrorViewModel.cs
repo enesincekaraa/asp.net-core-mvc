1b6315e79813bb9788332a54929edbfdc7ecbc7b
unknown <enesincekara61@gmail.com>
2023-06-14 16:57:28 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IncekaraAkademi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
